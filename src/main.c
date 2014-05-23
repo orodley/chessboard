166e7287649915723b8f7a34ddd2c4bba0a7a4b9
Owen Rodley <Strigoides@gmail.com>
2014-05-23 18:48:02 +1200

    Gracefully terminate when the window is closed.

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
 
 	GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(window), PROG_NAME);
+	g_signal_connect(G_OBJECT(window), "destroy",
+			G_CALLBACK(gtk_main_quit), NULL);
 	gtk_widget_show(window);
 
 	gtk_main();
