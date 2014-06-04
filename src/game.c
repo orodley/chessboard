76f4c2bc3ff9644af91eb882c4fb33b2af8d9fbf
Owen Rodley <Strigoides@gmail.com>
2014-06-04 17:52:04 +1200

    Check if a move is already in the tree, don't add duplicate moves.

--- src/game.c
+++ src/game.c
@@ -21,24 +21,29 @@ Game *new_game()
 
 Game *add_child(Game *game, Move move, Board *board)
 {
-	Game *new_node = new_game();
-	new_node->move = move;
-	new_node->board = board;
-	new_node->parent = game;
-
 	Game_list *children = game->children;
-	while (children->game != NULL && children->next != NULL)
+	while (children->game != NULL && children->next != NULL &&
+			children->game->move != move)
 		children = children->next;
 
-	if (children->game == NULL) {
-		children->game = new_node;
+	if (children->game != NULL && children->game->move == move) {
+		return children->game;
 	} else {
-		Game_list *list = malloc(sizeof(Game_list));
-		list->game = new_node;
-		children->next = list;
-	}
+		Game *new_node = new_game();
+		new_node->move = move;
+		new_node->board = board;
+		new_node->parent = game;
 
-	return new_node;
+		if (children->game == NULL) {
+			children->game = new_node;
+		} else {
+			Game_list *list = malloc(sizeof(Game_list));
+			list->game = new_node;
+			children->next = list;
+		}
+
+		return new_node;
+	}
 }
 
 void free_game(Game *game)
