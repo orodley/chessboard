63ca7a05ab1b27bb52649e5c0fc48832e062c926
Owen Rodley <Strigoides@gmail.com>
2014-06-13 16:01:21 +1200

    Improve PGN test system.
    Not all PGNs should be written out exactly the way they are read in.
    Modify the test system such that we specify an input and output format
    for each test.

--- test/pgn/run-tests.sh
+++ test/pgn/run-tests.sh
@@ -1,14 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
 cd `dirname $0`
 
-pgns=$(find . -type f -name '*.pgn')
+pgns=$(find . -type f -name '*.in')
+num_tests=0
+passed=0
+failed=0
 
 for pgn in $pgns; do
-	./test-pgn $pgn | diff - $pgn
-	if [ $? -ne 0 ]; then
-		echo
-		echo Failed on `basename $pgn`
-		exit 1
+	out=$(echo -n $pgn | sed 's/in$/out/')
+	if [ -e "$out" ]; then
+		echo "Testing $pgn..."
+		num_tests=$((tests_run+1))
+
+		./test-pgn "$pgn" | diff - "$out"
+		if [ $? -ne 0 ]; then
+			echo
+			echo Failed on `basename "$pgn"`
+
+			failed=$((failed+1))
+		else
+			passed=$((passed+1))
+		fi
 	fi
 done
+
+echo
+
+if [ $num_tests -eq $passed ]; then
+	echo "All $num_tests tests passed"
+else
+	echo "$failed / $num_tests tests failed"
+	exit 1
+fi
